a2286daefe9030b5a97cea83c23810bf68f7fc3f
PuzzleDude98 <puzzledude98@gmail.com>
2025-10-14 16:33:58 -0600

    Initial commit

---
+++ src/client/java/net/pd98/arbys_simulator/ArbysSimulatorClient.java
@@ -0,0 +1,10 @@
+package net.pd98.arbys_simulator;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArbysSimulatorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
